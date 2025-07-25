be49efd6677f9655ae3d1239db353534961ca709
ArunAK4 <arunkumara619@gmail.com>
2025-07-25 21:11:28 +0530

    uc10-api-gateway

--- usecase-10/modules/api-gateway/main.tf
+++ usecase-10/modules/api-gateway/main.tf
@@ -27,5 +27,14 @@ resource "aws_api_gateway_integration" "proxy_http" {
 
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = var.stage_name
+  depends_on = [
+    aws_api_gateway_integration.proxy_http
+  ]
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  rest_api_id        = aws_api_gateway_rest_api.api.id
+  deployment_id      = aws_api_gateway_deployment.deployment.id
+  stage_name         = var.stage_name
+  description        = "Stage for proxy integration"
 }
