96f348d0ff134da987a05c51c0207b2e156d8441
ArunAK4 <arunkumara619@gmail.com>
2025-07-28 06:49:37 +0530

    cognito

--- usecase-10/modules/cognito/main.tf
+++ usecase-10/modules/cognito/main.tf
@@ -2,19 +2,3 @@
 resource "aws_cognito_user_pool" "default" {
   name = "default-user-pool"
 }
-
-resource "aws_cognito_user_pool_client" "default" {
-  name         = "default-client"
-  user_pool_id = aws_cognito_user_pool.default.id
-
-  allowed_oauth_flows_user_pool_client = true
-  allowed_oauth_flows                  = ["code"]
-  allowed_oauth_scopes                 = ["email"]
-  supported_identity_providers         = ["COGNITO"]
-  callback_urls                        = [var.callback_url]
-}
-
-resource "aws_cognito_user_pool_domain" "default" {
-  domain       = var.domain_prefix
-  user_pool_id = aws_cognito_user_pool.default.id
-}
