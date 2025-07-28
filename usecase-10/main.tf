1214d918f90ff28809a95ea8223989553c9bf965
ArunAK4 <arunkumara619@gmail.com>
2025-07-28 07:04:19 +0530

    cognito

--- usecase-10/main.tf
+++ usecase-10/main.tf
@@ -4,5 +4,5 @@ module "cognito" {
   source         = "./modules/cognito"
   user_pool_name = "my-simple-user-pool"
   callback_url   = "https://3b5ugx8tla.execute-api.ap-south-1.amazonaws.com/dev"
-  domain_prefix = "https://myuniquedomain123.auth.ap-south-1.amazoncognito.com"
+  domain_prefix = "usecase10"
 }
