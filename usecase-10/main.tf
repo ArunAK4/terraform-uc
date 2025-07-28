975e7b338757ca31fffe483d813d431a0e1709e5
ArunAK4 <arunkumara619@gmail.com>
2025-07-28 06:57:50 +0530

    cognito

--- usecase-10/main.tf
+++ usecase-10/main.tf
@@ -2,5 +2,6 @@
 
 module "cognito" {
   source         = "./modules/cognito"
+  user_pool_name = "my-simple-user-pool"
   callback_url   = "https://3b5ugx8tla.execute-api.ap-south-1.amazonaws.com/dev"
 }
