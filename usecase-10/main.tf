814fb1d68798321a3a880eceb0e0482113af77ac
ArunAK4 <arunkumara619@gmail.com>
2025-07-28 10:55:20 +0530

    nlb

--- usecase-10/main.tf
+++ usecase-10/main.tf
@@ -30,7 +30,7 @@ module "alb" {
   source         = "./modules/alb"
   name           = "uc10-alb"
   vpc_id         = module.vpc.vpc_id
-  public_subnets = module.vpc.public_subnet_ids
+  public_subnets = module.vpc.private_subnet_ids
   alb_sg_id      = module.sg.alb_sg_id
 }
 
