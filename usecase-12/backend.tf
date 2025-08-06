2ef39c94fc65639403f8fdc20b35919eaf7f4a30
ArunAK <74036416+ArunAK4@users.noreply.github.com>
2025-08-06 15:23:02 +0530

    Update backend.tf

--- usecase-12/backend.tf
+++ usecase-12/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "terraform-usecases-batch6"
-    key          = "usecase10/terraform.tfstate"
+    key          = "usecase12/terraform.tfstate"
     region       = "eu-west-1"
     use_lockfile = true
   }
